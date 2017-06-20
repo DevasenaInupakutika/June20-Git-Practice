62e4c727b841108ab7cd93079bb3d7666d554da0
Devasena Inupakutika <devasena.prasad@gmail.com>
2017-06-20 11:08:31 -0400

    Added some more comments to the code

--- patient-analysis.py
+++ patient-analysis.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 
 data = np.loadtxt(fname='data/inflammation1.csv',delimiter=',')
 
+#Finding dimensions of data
+print(data.shape)
+
 print(data)
 
+#Plotting data
 image-1=plt.plot(data)
 
 plt.show(image-1)
